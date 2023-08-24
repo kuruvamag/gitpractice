32ac241bfafdc6fa24f738100bb1129562060085
magnitia <magnitia@192.168.0.102>
2023-08-24 11:48:37 +0530

    messgae

---
+++ src/TestRunner.java
@@ -0,0 +1,22 @@
+
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+
+		/*
+		 * System.setProperty("webdriver.chrome.driver",
+		 * "C:\\Users\\magnitia\\Downloads\\chromedriver.exe"); WebDriver driver = new
+		 * ChromeDriver();
+		 * 
+		 * driver.get(
+		 * "https://www.magnitia.com/images/courses/Selenium-with-Java-Magnitia-Content.pdf"
+		 * ); //driver.get("https://selectorshub.com/xpath-practice-page");
+		 * driver.manage().window().maximize(); Thread.sleep(15000);
+		 */
+		int x = 10 ;
+		System.out.println(x);
+
+	}
+
+}
